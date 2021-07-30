d56e8fe6b6ef06d659b0185275c9cc524528fb93
HiyaTaunk <77522311+HiyaTaunk@users.noreply.github.com>
2021-07-30 15:44:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+//Create variables here
+var dog,happyDog;
+var database,foodS,foodStock;
+
+function preload()
+{
+	//load images here
+  dogImg =  loadImage("images/Dog.png");
+  happydogImg = loadImage("images/happydog.png");
+}
+
+function setup() {
+  database = firebase.database();
+
+	createCanvas(500, 500);
+
+  dog = createSprite(250,380);
+  dog.addImage(dogImg);
+  dog.scale = 0.2;
+
+  //add = createButton("ADD FOOD");
+ // add.position(750,184);
+  //add.mousePressed(addStock);
+ 
+  
+
+//the data base is added 
+foodStock = database.ref('food');
+foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+background(0,255,255);
+
+if(keyWentDown("UP_ARROW")){
+  WriteStock(foodS);
+  dog.addImage("happy",happydogImg);
+  
+   
+}
+
+  drawSprites();
+  //add styles here
+  textSize(25);
+  fill("red");
+  stroke("cyan");
+  text("food remaining = "+ foodS,130,80);
+  text("press upArrow to feed the dog 🐶 " ,90 , 120 );
+  text("My dog's name is Archie 💖",130,160);
+  
+}
+
+function readStock(data){
+  foodS = data.val();
+
+}
+
+function WriteStock(x){
+ if(x<=0){
+   x = 0;
+ }
+ else{
+   x = x-1;
+ }
+  database.ref('/').update({
+      food:x
+  })
+}
+
+
+ 
